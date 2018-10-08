3911b819f37fd72100751cda0281d48225a07eb4
Drew Thayer <thedrewthayer@gmail.com>
2018-10-08 10:36:18 -0600

    remove scripts dir

--- src/scripts/pymongo_scripts.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-def docs_from_mongodb_collection(db, coll_name):
-    coll = db[coll_name]
-    docs = []
-    for doc in coll.find():
-        docs.append(doc)
-    return docs
